ce72ee9988525cf61bf91afa9ed3dfad52ad0374
the projects <84352313+theorchidproject@users.noreply.github.com>
2023-02-27 10:24:07 +0000

    OrchidOs revert add android camera go
    will remove cam for now as arrow gms has camera included

--- device.mk
+++ device.mk
@@ -169,7 +169,14 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    GCamGO
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    Snap \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Component overrides
 PRODUCT_COPY_FILES += \
